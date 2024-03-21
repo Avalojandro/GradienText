08b9c44a0cd91aa89fcba11a3b82bd276f179955
avalojandro <avalojandro@gmail.com>
2024-03-21 17:21:34 -0600

    :art: code cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import "./App.css";
 
+import IconGithub from './assets/icons/github'
+
 function App() {
+
+
+  const test = `.gradient-text { 
+    background-color: red;
+    background-image: linear-gradient(45deg, #f3ec78, #af4261);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    background-image: linear-gradient(45deg, #f3ec78, #af4261);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+  }`
+
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -15,16 +33,40 @@ function App() {
             GRADIENTEXT
           </div>
           <div className="text-center mt-4 font-extrabold text-2xl">
-            A css guide for your gradient texts
+            A CSS guide for your gradient texts
           </div>
-          <div>
-            <a href="">
+          <div className="text-center flex my-4 justify-center gap-4">
+            <a className="bg-cyan-500 px-4 py-2 rounded-lg">
               Github
+            </a>
+            <a className="bg-cyan-500 px-4 py-2 rounded-lg">
+              About
             </a>
           </div>
         </div>
       </div>
-      <div className="h-96 bg-black"></div>
+      <div className=" bg-black justify-center flex">
+      <div className="max-w-[50%] w-full py-8">
+      
+
+        <div className="text-center flex my-12">
+            <div className="bg-[#2B2B2B] text-white w-full max-w-64 py-20 px-12 rounded-tl-2xl rounded-bl-2xl">
+              <span className="archivo-black-regular text-7xl gradient-text">1.</span>
+            </div>
+            <div className="bg-[#19191a] overflow-y-auto text-left p-4 w-full rounded-tr-2xl rounded-br-2xl">
+              <pre>
+              <code className="gradient-text">
+                {test}
+              </code>
+              </pre>
+            </div>
+        </div>
+
+
+        
+        
+      </div>
+      </div>
     </>
   );
 }
